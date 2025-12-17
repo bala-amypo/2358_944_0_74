14de32a832fe65194115df85be4c5949e817bdb1
bala-amypo <bala@amypo.in>
2025-12-17 08:04:31 +0000

    User commit

--- src/main/java/com/newentity/NewFileEntity.java
+++ src/main/java/com/newentity/NewFileEntity.java
@@ -1,12 +1,13 @@
 package com.example.demo.newentity;
 
 import jakarta.persistence.Id;
-import jakarta.validation. constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
-public class NewfileEntity{
+public class NewfileEntity {
 @Id
 private Long id;
-@NotBlank
+@NotBlank (message = "should not contain spaces")
 private String name;
 private String email;
+
 }

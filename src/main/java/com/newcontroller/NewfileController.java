affe8b1ed5de0555362831a6ce918a4a6e2698db
bala-amypo <bala@amypo.in>
2025-12-17 10:29:21 +0000

    User commit

--- src/main/java/com/newcontroller/NewfileController.java
+++ src/main/java/com/newcontroller/NewfileController.java
@@ -1,6 +1,33 @@
-package com.example.demo.newcontroller;
+package.com.example.demo.newcontroller;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation .*;
+import com.example.demo.entity.Student;
+import com.example.demo.newentity.NewfileEntity;
+import com.example.demo.newservice.NewfileService;
+import java.util.List;
+@RequestMapping("/students")
+public class StudentController {
+private final NewfileService service;
+public StudentController(NewfileService service) {
+this.service = service;
 
-public class NewfileController{
+@PostMapping
+public ResponseEntity<Student> createStudent(@Valid @RequestBodyStudent service) {
+return new ResponseEntity<>(service.saveStudent(student),HttpStatus.CREATED);
 
+@GetMapping
+public List<NewfileEntity> getall() {
+return service.getall();
 
+@GetMapping("/{id]")
+public NewfileEntity getStudent(@PathVariable Long id) {
+return service.getidval(id);
+
+@PutMapping("/{id]")
+public NewfileEntity updateStudent(@PathVariable Long id,@Valid @RequestBody Student student){
+return service. update(id, student);
+
+}
 }

5fba31b50831ac01545d562dd34391455f9a8a0a
bala-amypo <bala@amypo.in>
2025-12-17 10:34:19 +0000

    User commit

--- src/main/java/com/newcontroller/NewfileController.java
+++ src/main/java/com/newcontroller/NewfileController.java
@@ -1,8 +1,8 @@
-package.com.example.demo.newcontroller;
+package com.exampie.demo.newcontroller;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation .*;
+import org.springframework.web.bind.annotation.";
 import com.example.demo.entity.Student;
 import com.example.demo.newentity.NewfileEntity;
 import com.example.demo.newservice.NewfileService;
@@ -12,22 +12,21 @@ public class StudentController {
 private final NewfileService service;
 public StudentController(NewfileService service) {
 this.service = service;
-
+}
 @PostMapping
-public ResponseEntity<Student> createStudent(@Valid @RequestBodyStudent service) {
+public ResponseEntity<Student> createStudent(@Valid @RequestBodyStudent service){
 return new ResponseEntity<>(service.saveStudent(student),HttpStatus.CREATED);
-
+}
 @GetMapping
 public List<NewfileEntity> getall() {
 return service.getall();
-
-@GetMapping("/{id]")
+}
+@GetMapping("/(id}")
 public NewfileEntity getStudent(@PathVariable Long id) {
 return service.getidval(id);
-
-@PutMapping("/{id]")
-public NewfileEntity updateStudent(@PathVariable Long id,@Valid @RequestBody Student student){
-return service. update(id, student);
-
+}
+@PutMapping("/(id}")
+public Student updateStudent(@PathVariable Long id,@Valid @RequestBody Student student){
+return service.update(id, student);
 }
 }

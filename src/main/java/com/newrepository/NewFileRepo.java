287b93f0c8fe8432d2feb52d8bee2719b277b710
bala-amypo <bala@amypo.in>
2025-12-20 17:41:51 +0000

    User commit

--- src/main/java/com/newrepository/NewFileRepo.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.newrepository;
-import org. springframework.data. jpa.repository. JpaRepository;
-import com.example.demo.newentity.NewfileEntity;
-public interface NewfileRepo extends JpaRepository<NewfileEntity, Long>{
-
-}
